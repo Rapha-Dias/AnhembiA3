90772025479c229a9a42ea3b0350581f8ca9a376
João Henrique de Souza Mór <98439417+JhonasCB@users.noreply.github.com>
2023-05-29 20:22:14 -0300

    Finalizando Client.java

--- Client.java
+++ Client.java
@@ -40,3 +40,84 @@ public class Client {
             JOptionPane.showMessageDialog(null, "Erro ao ler entrada do usuário: " + e.getMessage());
         }
     }
+    
+    private void run() {
+        try (Socket socket = new Socket(HOST, PORT)) {
+            System.out.println("Conectado ao servidor");
+            GameGUI game = new GameGUI(socket);
+            game.setVisible(true);
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(null, "Erro ao conectar ao servidor: " + e.getMessage());
+        }
+    }
+
+    static class GameGUI extends JFrame {
+        private BufferedReader in;
+        private PrintWriter out;
+        private JTextField inputField;
+        private JButton playButton;
+        private JLabel statusLabel;
+        private JLabel scoreLabel;
+        private JComboBox<String> paritySelector;
+
+        public GameGUI(Socket socket) {
+            try {
+                in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                out = new PrintWriter(socket.getOutputStream(), true);
+            } catch (IOException e) {
+                JOptionPane.showMessageDialog(null, "Erro ao configurar fluxos de entrada/saída: " + e.getMessage());
+            }
+
+            setTitle("Par ou Ímpar");
+            setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            setSize(350, 200);
+            setLayout(new BorderLayout(10, 10));
+
+            JPanel topPanel = new JPanel();
+            topPanel.setLayout(new FlowLayout());
+            topPanel.add(new JLabel("Selecione Par ou Ímpar:"));
+            paritySelector = new JComboBox<>(new String[] { "Par", "Ímpar" });
+            topPanel.add(paritySelector);
+            topPanel.add(new JLabel("Digite um número:"));
+            inputField = new JTextField(10);
+            topPanel.add(inputField);
+            playButton = new JButton("Jogar");
+            topPanel.add(playButton);
+            add(topPanel, BorderLayout.NORTH);
+
+            JPanel centerPanel = new JPanel(new BorderLayout());
+            statusLabel = new JLabel("", SwingConstants.CENTER);
+            centerPanel.add(statusLabel, BorderLayout.CENTER);
+
+            JPanel scorePanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+            scoreLabel = new JLabel("Placar: 0 - 0");
+            scorePanel.add(scoreLabel);
+            centerPanel.add(scorePanel, BorderLayout.SOUTH);
+
+            add(centerPanel, BorderLayout.CENTER);
+
+            playButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    String input = inputField.getText();
+                    if (!input.matches("\\d+")) {
+                        JOptionPane.showMessageDialog(null, "Por favor, digite um número válido.");
+                        return;
+                    }
+                    String parity = (String) paritySelector.getSelectedItem();
+                    out.println(parity + "," + input);
+                    try {
+                        String response = in.readLine();
+                        String[] parts = response.split(",");
+                        statusLabel.setText(parts[0]);
+                        scoreLabel.setText(parts[1]);
+                    } catch (IOException ex) {
+                        JOptionPane.showMessageDialog(null, "Erro ao ler do servidor: " + ex.getMessage());
+                    }
+                }
+            });
+
+            getRootPane().setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        }
+    }
+}

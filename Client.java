c9eb7af3b2ed85cb7f617c223542415395c8e4ba
Raphael Dias <42981400+Rapha-Dias@users.noreply.github.com>
2023-05-29 20:28:47 -0300

    correção erro Socket

--- Client.java
+++ Client.java
@@ -1,6 +1,5 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
 import java.io.*;
 import java.net.*;
 
@@ -17,37 +16,45 @@ public class Client {
     }
 
     private void initGame() {
+        System.out.println("====================== PAR OU IMPAR ======================");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("\nAntes de começar, por favor, diga-nos seu nome de usuário: ");
         try {
-            System.out.println("====================== PAR OU IMPAR ======================");
-            System.out.print("\nAntes de começar, por favor, diga-nos seu nome de usuário: ");
-            usuario = new BufferedReader(new InputStreamReader(System.in)).readLine();
-
-            System.out.println("\n\t============== BEM VINDO(A)" + usuario.toUpperCase() + " ==============");
-            System.out.println("\n\t   Algumas informações que você precisa saber");
-
-            System.out.println("\n==============================================================");
-            System.out.println("\n\t  A3 ANHEMBI FOI DESENVOLVIDO POR: ");
-            System.out.println("\n\t  João Henrique de Souza Mó     RA 125111345893");
-            System.out.println("\t    Igor Cesar Sanches Silva      RA 125111368951");
-            System.out.println("\t    Murillo Cesar Bispo Ferreira  RA 125111370656");
-            System.out.println("\t    Nathasha Lohanne Kirdeikas    RA 125111374719");
-            System.out.println("\t    Raphael Ferreira Dias         RA 125111347434");
-            System.out.println("\t    Davi Oliveira dos Santos      RA 125111350411");
-            System.out.println("\n==============================================================");
-            System.out.println("\n\t     APERTE 'ENTER' PARA CONTINUAR ");
-            new BufferedReader(new InputStreamReader(System.in)).readLine();
+            usuario = reader.readLine();
         } catch (IOException e) {
-            JOptionPane.showMessageDialog(null, "Erro ao ler entrada do usuário: " + e.getMessage());
+            System.err.println("Erro ao ler entrada do usuário: " + e.getMessage());
+            System.exit(-1);
+        }
+
+        System.out.printf("\n\t============== BEM VINDO(A) %s ==============\n", usuario.toUpperCase());
+        System.out.println("\n\t   Algumas informações que você precisa saber");
+
+        System.out.println("\n==============================================================");
+        System.out.println("\n\t  A3 ANHEMBI FOI DESENVOLVIDO POR: ");
+        System.out.println("\n\t  João Henrique de Souza Mó     RA 125111345893");
+        System.out.println("\t    Igor Cesar Sanches Silva      RA 125111368951");
+        System.out.println("\t    Murillo Cesar Bispo Ferreira  RA 125111370656");
+        System.out.println("\t    Nathasha Lohanne Kirdeikas    RA 125111374719");
+        System.out.println("\t    Raphael Ferreira Dias         RA 125111347434");
+        System.out.println("\t    Davi Oliveira dos Santos      RA 125111350411");
+        System.out.println("\n==============================================================");
+        System.out.println("\n\t     APERTE 'ENTER' PARA CONTINUAR ");
+        try {
+            reader.readLine();
+        } catch (IOException e) {
+            System.err.println("Erro ao ler entrada do usuário: " + e.getMessage());
+            System.exit(-1);
         }
     }
-    
+
     private void run() {
         try (Socket socket = new Socket(HOST, PORT)) {
             System.out.println("Conectado ao servidor");
             GameGUI game = new GameGUI(socket);
             game.setVisible(true);
         } catch (IOException e) {
-            JOptionPane.showMessageDialog(null, "Erro ao conectar ao servidor: " + e.getMessage());
+            System.err.println("Erro ao conectar ao servidor: " + e.getMessage());
+            System.exit(-1);
         }
     }
 
@@ -65,7 +72,8 @@ public class Client {
                 in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                 out = new PrintWriter(socket.getOutputStream(), true);
             } catch (IOException e) {
-                JOptionPane.showMessageDialog(null, "Erro ao configurar fluxos de entrada/saída: " + e.getMessage());
+                System.err.println("Erro ao configurar fluxos de entrada/saída: " + e.getMessage());
+                System.exit(-1);
             }
 
             setTitle("Par ou Ímpar");
@@ -96,24 +104,22 @@ public class Client {
 
             add(centerPanel, BorderLayout.CENTER);
 
-            playButton.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    String input = inputField.getText();
-                    if (!input.matches("\\d+")) {
-                        JOptionPane.showMessageDialog(null, "Por favor, digite um número válido.");
-                        return;
-                    }
-                    String parity = (String) paritySelector.getSelectedItem();
-                    out.println(parity + "," + input);
-                    try {
-                        String response = in.readLine();
-                        String[] parts = response.split(",");
-                        statusLabel.setText(parts[0]);
-                        scoreLabel.setText(parts[1]);
-                    } catch (IOException ex) {
-                        JOptionPane.showMessageDialog(null, "Erro ao ler do servidor: " + ex.getMessage());
-                    }
+            playButton.addActionListener(e -> {
+                String input = inputField.getText();
+                if (!input.matches("\\d+")) {
+                    JOptionPane.showMessageDialog(null, "Por favor, digite um número válido.");
+                    return;
+                }
+                String parity = (String) paritySelector.getSelectedItem();
+                out.println(parity + "," + input);
+                try {
+                    String response = in.readLine();
+                    String[] parts = response.split(",");
+                    statusLabel.setText(parts[0]);
+                    scoreLabel.setText(parts[1]);
+                } catch (IOException ex) {
+                    System.err.println("Erro ao ler do servidor: " + ex.getMessage());
+                    System.exit(-1);
                 }
             });
 
--- Server.java
+++ Server.java
@@ -1,4 +1,3 @@
-// Server.java
 import java.io.*;
 import java.net.*;
 import java.util.Random;
@@ -10,40 +9,63 @@ public class Server {
         try (ServerSocket serverSocket = new ServerSocket(PORT)) {
             System.out.println("Server is listening on port " + PORT);
             while (true) {
-                try (Socket socket = serverSocket.accept()) {
-                    System.out.println("New client connected");
-                    PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-                    BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                    Random rand = new Random();
-                    int scorePlayer = 0;
-                    int scoreComputer = 0;
-
-                    while (true) {
-                        String input = in.readLine();
-                        if (input == null || input.isEmpty()) {
-                            break;
-                        }
-                        String[] parts = input.split(",");
-                        String parity = parts[0];
-                        int playerNumber = Integer.parseInt(parts[1]);
-                        int computerNumber = rand.nextInt(6); 
-                        int total = playerNumber + computerNumber;
-                        boolean isTotalEven = total % 2 == 0;
-                        boolean playerWins = (parity.equals("Par") && isTotalEven) || (parity.equals("Ímpar") && !isTotalEven);
-
-                        if (playerWins) {
-                            scorePlayer++;
-                        } else {
-                            scoreComputer++;
-                        }
-
-                        String result = playerWins ? "Você ganhou!" : "Você perdeu!";
-                        out.println(result + ",Placar: " + scorePlayer + " - " + scoreComputer);
-                    }
-                }
+                new ClientHandler(serverSocket.accept()).start();
             }
         } catch (IOException e) {
-            System.out.println("Error in server: " + e.getMessage());
+            System.err.println("Error in server: " + e.getMessage());
+            System.exit(-1);
+        }
+    }
+
+    static class ClientHandler extends Thread {
+        private Socket socket;
+        private BufferedReader in;
+        private PrintWriter out;
+
+        ClientHandler(Socket socket) {
+            this.socket = socket;
+        }
+
+        public void run() {
+            try {
+                in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                out = new PrintWriter(socket.getOutputStream(), true);
+                System.out.println("New client connected");
+                Random rand = new Random();
+                int scorePlayer = 0;
+                int scoreComputer = 0;
+
+                while (true) {
+                    String input = in.readLine();
+                    if (input == null || input.isEmpty()) {
+                        break;
+                    }
+                    String[] parts = input.split(",");
+                    String parity = parts[0];
+                    int playerNumber = Integer.parseInt(parts[1]);
+                    int computerNumber = rand.nextInt(6);
+                    int total = playerNumber + computerNumber;
+                    boolean isTotalEven = total % 2 == 0;
+                    boolean playerWins = (parity.equals("Par") && isTotalEven) || (parity.equals("Ímpar") && !isTotalEven);
+
+                    if (playerWins) {
+                        scorePlayer++;
+                    } else {
+                        scoreComputer++;
+                    }
+
+                    String result = playerWins ? "Você ganhou!" : "Você perdeu!";
+                    out.println(result + ",Placar: " + scorePlayer + " - " + scoreComputer);
+                }
+            } catch (IOException e) {
+                System.err.println("Error in client handler: " + e.getMessage());
+            } finally {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    System.err.println("Error while closing socket: " + e.getMessage());
+                }
+            }
         }
     }
 }
